42c53dd9962cabc587fcb8fb44028de6288249ec
Chun-Ying Huang <huangant@gmail.com>
2026-01-28 21:47:35 +0800

    revised packaging commands

--- packaging/msix.cmd
+++ packaging/msix.cmd
@@ -7,13 +7,14 @@ curl -L -o "msix\hashsumr-amd64.exe" "https://github.com/hashsumr/hashsumr/relea
 curl -L -o "msix\hashsumr-arm64.exe" "https://github.com/hashsumr/hashsumr/releases/download/v0.0.2/hashsumr-arm64.exe"
 
 @REM packaging
-makeappx pack /f map_amd64.txt /p hashsumr_amd64.msix
-makeappx pack /f map_arm64.txt /p hashsumr_arm64.msix
+mkdir output
+makeappx pack /f map_amd64.txt /p .\output\hashsumr_amd64.msix
+makeappx pack /f map_arm64.txt /p .\output\hashsumr_arm64.msix
 
 @REM sign the msix
-signtool sign /fd SHA256 /a /f ".\hashsumr-dev.pfx" /p "%password%" hashsumr_amd64.msix
-signtool sign /fd SHA256 /a /f ".\hashsumr-dev.pfx" /p "%password%" hashsumr_arm64.msix
+signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\output\hashsumr_amd64.msix
+signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\output\hashsumr_arm64.msix
 
 @REM create and sign the bundle
-makeappx bundle /f bundlemap.txt /p hashsumr.msixbundle
-signtool sign /fd SHA256 /a /f ".\hashsumr-dev.pfx" /p "%password%" hashsumr.msixbundle
+makeappx bundle /f bundlemap.txt /p .\output\hashsumr.msixbundle
+signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\output\hashsumr.msixbundle
