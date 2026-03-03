8b4a0ead3a17f96bd694ed81200862471e0b6182
Chun-Ying Huang <huangant@gmail.com>
2026-03-03 11:58:49 +0800

    set version number automatically on Windows

--- packaging/msix.cmd
+++ packaging/msix.cmd
@@ -7,6 +7,10 @@
 curl -L -o "msix\app\hashsumr-amd64.exe" "https://github.com/hashsumr/hashsumr/releases/download/v%version%/hashsumr-amd64.exe"
 curl -L -o "msix\app\hashsumr-arm64.exe" "https://github.com/hashsumr/hashsumr/releases/download/v%version%/hashsumr-arm64.exe"
 
+@REM set version
+powershell -Command "(Get-Content .\msix\app\AppxManifest_amd64.xml) -replace '\$version\$', '%version%.0' | Set-Content .\msix\app\AppxManifest_amd64.xml"
+powershell -Command "(Get-Content .\msix\app\AppxManifest_arm64.xml) -replace '\$version\$', '%version%.0' | Set-Content .\msix\app\AppxManifest_arm64.xml"
+
 @REM packaging
 mkdir output
 makeappx pack /f .\msix\map_amd64.txt /p .\output\hashsumr_amd64.msix
@@ -17,5 +21,5 @@ signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\outpu
 signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\output\hashsumr_arm64.msix
 
 @REM create and sign the bundle
-makeappx bundle /f .\msix\bundlemap.txt /p .\output\hashsumr.msixbundle
+makeappx bundle /f .\msix\bundlemap.txt /p .\output\hashsumr.msixbundle /bv "%version%.0"
 signtool sign /fd SHA256 /a /f ".\cert\chunying-dev.pfx" /p "%password%" .\output\hashsumr.msixbundle
